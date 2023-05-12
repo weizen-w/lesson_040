bc62f4a8c83d540de3f364403b21581aba90059b
Wladimir <weizen.w@gmx.de>
2023-05-12 10:19:21 +0200

    feat: add class Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+  public static void main(String[] args) {
+    Superman superman = new Superman();
+    System.out.println("Супермен " + superman.fly());
+    System.out.println("Супермен " + superman.run());
+    System.out.println("Супермен " + superman.swim());
+  }
+
+}
