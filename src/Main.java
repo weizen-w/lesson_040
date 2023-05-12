f53d267d06b1d38717bba59b3561b0dca18b679c
Wladimir <weizen.w@gmx.de>
2023-05-12 10:29:26 +0200

    feat: add class Main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ public class Main {
 
   public static void main(String[] args) {
     Superman superman = new Superman();
-    System.out.println("Супермен " + superman.fly());
-    System.out.println("Супермен " + superman.run());
-    System.out.println("Супермен " + superman.swim());
+    System.out.println(superman.fly());
+    System.out.println(superman.run());
+    System.out.println(superman.swim());
+    Airplane airplane = new Airplane();
+    System.out.println(airplane.fly());
+    Duck duck = new Duck();
+    System.out.println(duck.fly());
+    System.out.println(duck.run());
+    System.out.println(duck.swim());
+    Penguin penguin = new Penguin();
+    System.out.println(penguin.run());
+    System.out.println(penguin.swim());
+    People people = new People();
+    System.out.println(people.run());
+    System.out.println(people.swim());
   }
-
 }
