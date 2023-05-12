b26c9e04e07be163d603f2055a6c66f4621b01a4
Wladimir <weizen.w@gmx.de>
2023-05-12 19:56:59 +0200

    fix: refactor Main, add Main.makeThem* methods

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,61 @@
+import interfaces.CanFly;
+import interfaces.CanRun;
+import interfaces.CanSwim;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
     Superman superman = new Superman();
-    System.out.println(superman.fly());
-    System.out.println(superman.run());
-    System.out.println(superman.swim());
     Airplane airplane = new Airplane();
-    System.out.println(airplane.fly());
     Duck duck = new Duck();
-    System.out.println(duck.fly());
-    System.out.println(duck.run());
-    System.out.println(duck.swim());
     Penguin penguin = new Penguin();
-    System.out.println(penguin.run());
-    System.out.println(penguin.swim());
     People people = new People();
-    System.out.println(people.run());
-    System.out.println(people.swim());
+    System.out.println();
+    List<CanFly> flyable = new ArrayList<>();
+    flyable.add(superman);
+    flyable.add(airplane);
+    flyable.add(duck);
+    makeThemFly(flyable);
+    System.out.println();
+    List<CanRun> runable = new ArrayList<>();
+    runable.add(superman);
+    runable.add(duck);
+    runable.add(penguin);
+    runable.add(people);
+    makeThemRun(runable);
+    System.out.println();
+    List<CanSwim> swimmable = new ArrayList<>();
+    swimmable.add(superman);
+    swimmable.add(duck);
+    swimmable.add(penguin);
+    swimmable.add(people);
+    makeThemSwim(swimmable);
+    System.out.println();
+  }
+
+  public static void makeThemFly(List<CanFly> flyable) {
+    System.out.println("Main.makeThemFly(flyable=" + flyable + ")");
+    for (CanFly flyableObject : flyable) {
+      System.out.println(flyableObject + ".fly()");
+      flyableObject.fly();
+    }
+  }
+
+  public static void makeThemRun(List<CanRun> runable) {
+    System.out.println("Main.makeThemRun(runable=" + runable + ")");
+    for (CanRun runableObject : runable) {
+      System.out.println(runableObject + ".run()");
+      runableObject.run();
+    }
+  }
+
+  public static void makeThemSwim(List<CanSwim> swimmable) {
+    System.out.println("Main.makeThemSwim(swimmable=" + swimmable + ")");
+    for (CanSwim swimmableObject : swimmable) {
+      System.out.println(swimmableObject + ".swim()");
+      swimmableObject.swim();
+    }
   }
 }
